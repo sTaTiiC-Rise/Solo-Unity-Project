c0dd0301b716f076fbaee11617124d8ca6b3ffbd
Loan_Shark <26.hayes.c@gmail.com>
2025-09-08 10:21:42 -0700

    Camera Rotation and Larry

---
+++ Solo Unity Project/Assets/CameraRotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = -2.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(v, h, -1);
+    }
+}
