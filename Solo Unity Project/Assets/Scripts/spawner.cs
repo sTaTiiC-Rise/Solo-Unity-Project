b34ffb891e8efae82e87e5216a82497e85aeffc9
Loan_Shark <26.hayes.c@gmail.com>
2025-10-22 09:55:09 -0700

    fucking unity can suck my dick

--- Solo Unity Project/Assets/Scripts/spawner.cs
+++ Solo Unity Project/Assets/Scripts/spawner.cs
@@ -1,22 +1,54 @@
 using UnityEngine;
+using System.Collections;
 
 public class spawner : MonoBehaviour
 {
-    public GameObject enemyHolder;
+    //public GameObject enemyHolder;
+
+    public GameObject enemygrp;
+
+    public Transform pos;
+
+    public bool isDelaying = false;
+    public float delayTime;
+    public float delayIncrement = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        enemyHolder.SetActive(false);
+        // enemyHolder.SetActive(false);'
+        StartCoroutine(Retard());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isDelaying)
+            StartCoroutine(Retard());
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
-            enemyHolder.SetActive(true);
+       /*if (other.gameObject.tag == "Player")
+            enemyHolder.SetActive(true);*/
+    }
+    IEnumerator Retard()
+    {
+        Debug.Log("Started");
+
+        Instantiate(enemygrp, pos);
+        isDelaying = true;
+        Debug.Log("Spawned");
+        yield return new WaitForSeconds(delayTime);
+        if (delayTime >= 0.1)
+        {
+            delayTime -= delayIncrement;
+            delayIncrement += 0.5f;
+        }
+        else
+        {
+            delayTime = 0.1f;
+        }
+
+            isDelaying = false;
     }
 }
